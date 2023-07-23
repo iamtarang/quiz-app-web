5dc415aa9685bd679dee7d7a406e9fc7b4315365
Tarang Lakhmapurkar <tarangjlakh@gmail.com>
2023-07-23 10:51:42 +0530

    Layput done

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './homepage/Layout';
+import Login from './Login';
 
 function App() {
 
   return (
     <>
       <Routes>
+        <Route path='/login' element={<Login />} />
         <Route path='/dashboard' element={<Layout />} >
           <Route index element={<Dashboard />} />
         </Route>
